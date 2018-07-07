dc8abf80146f4895b3bc1ab4431e57cfed6ceabc
Renzo Castro <renzo.vista@gmail.com>
2018-07-07 10:29:29 -0500

    c3-2

--- clase03/main.js
+++ clase03/main.js
@@ -1,1 +1,15 @@
 console.log('JS ok!');
+
+
+const qsContenedor2 = document.querySelector('#contenedor2');
+const qsaHijos = document.querySelectorAll('#contenedor1 .hijo');
+
+console.log(qsContenedor2);
+console.log(qsaHijos);
+
+
+const contenedor1 = document.getElementById('contenedor1');
+const hijos = document.getElementsByClassName('hijo');
+
+console.log(contenedor1);
+console.log(hijos);
