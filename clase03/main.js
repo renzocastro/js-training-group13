32ad93cf05afa410d2618faaf77818fb69f67426
Renzo Castro <renzo.vista@gmail.com>
2018-07-07 10:53:31 -0500

    c3-3

--- clase03/main.js
+++ clase03/main.js
@@ -13,3 +13,18 @@ const hijos = document.getElementsByClassName('hijo');
 
 console.log(contenedor1);
 console.log(hijos);
+
+
+
+
+function toArray(elements) {
+  return Array.prototype.slice.call(elements, 0);
+}
+
+function qsa(selector) {
+  return toArray(document.querySelectorAll(selector));
+}
+
+const hijos2 = qsa('.hijo');
+// const hijos2 = $('.hijo'); // jQuery
+
