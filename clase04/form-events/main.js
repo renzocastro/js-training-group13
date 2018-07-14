cdbcd33c41725036d0775c14134556a40f4cdea3
Renzo Castro <renzo.vista@gmail.com>
2018-07-14 14:25:03 -0500

    C4-5

--- clase04/form-events/main.js
+++ clase04/form-events/main.js
@@ -1,19 +1,32 @@
 let txtNombre;
+let formDemo;
 
 const onClickBtn = event => {
   console.log('click:', txtNombre.value);
 };
 
 const onSubmitFormDemo = event => {
-  console.log('submit');
+  console.log('submit', formDemo);
 
   event.preventDefault();
 };
 
+const onFocusNombre = event => {
+  console.log('focus');
+  txtNombre.classList.remove('error');
+};
+
+const onBlurNombre = event => {
+  console.log('blur');
+  txtNombre.classList.add('error');
+};
+
 const init = () => {
   txtNombre = document.getElementById('txtNombre');
+  txtNombre.addEventListener('focus', onFocusNombre);
+  txtNombre.addEventListener('blur', onBlurNombre);
 
-  const formDemo = document.getElementById('formDemo');
+  formDemo = document.getElementById('formDemo');
   formDemo.addEventListener('submit', onSubmitFormDemo);
 
   const btn = document.getElementById('btnMostrar');
