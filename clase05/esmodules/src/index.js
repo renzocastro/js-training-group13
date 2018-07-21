314ea659733a1dd9479a48d35b97f29d8339f05c
Andres <an.dres.69@outlook.com>
2018-07-21 13:53:11 -0500

    Promise

--- clase05/esmodules/src/index.js
+++ clase05/esmodules/src/index.js
@@ -17,3 +17,50 @@ console.log( sumar(5, 3) ); // 8
 console.log( juntar("hola", "mundo") ); // 8
 
 
+
+
+// Promise
+// .resolve
+// .reject
+
+
+let p = new Promise( (resolve, reject) => {
+
+  setTimeout(() => { resolve('hola') }, 10000);
+
+} );
+
+p.then( (message) => {
+  console.log('pasaron 10 seg', message);
+} );
+
+
+
+
+
+function sumarPromise(a, b) {
+  const ejecutor = (resolve, reject) => {
+    setTimeout(() => { resolve(a + b) }, 10000);
+  };
+
+  const p = new Promise(ejecutor);
+
+  return p;
+}
+
+sumarPromise(3, 4).then( (resultado) => { console.log(resultado) } );
+
+
+function restarPromise(a, b) {
+  const ejecutor = (resolve, reject) => {
+    setTimeout(() => { resolve(a - b) }, 3000);
+  };
+
+  const p = new Promise(ejecutor);
+
+  return p;
+}
+
+restarPromise(5, 4).then( (resultado) => { console.log(resultado) } );
+
+
