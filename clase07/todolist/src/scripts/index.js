863c12707a9c768ac890de17c44e508a7b1120a3
Renzo Castro <renzo.vista@gmail.com>
2018-08-18 15:57:16 -0500

    C7-3

--- clase07/todolist/src/scripts/index.js
+++ clase07/todolist/src/scripts/index.js
@@ -14,25 +14,40 @@ const taskList = document.querySelector('.todolist__list');
 let taskCount = 0;
 
 
-const addTask = (task) => {
+const addTask = task => {
   taskCount++;
 
   const taskItemElement = document.createElement('div');
   taskItemElement.innerHTML = createTodoListItemHTML(taskCount, task);
 
-  const btnTaskRemove = taskItemElement.querySelector('.todolist__item__remove');
-  btnTaskRemove.addEventListener('click', onClickBtnTaskRemove);
+  // const btnTaskRemove = taskItemElement.querySelector('.todolist__item__remove');
+  // btnTaskRemove.addEventListener('click', onClickBtnTaskRemove);
 
   taskList.appendChild(taskItemElement);
+};
+
+const removeTaskFromButton = buttonNode => {
+  // const item = btn.parentElement();
+  const item = buttonNode.closest('.todolist__item');
+
+  if (item) {
+    item.remove();
+  }
 };
 
 const onClickBtnTaskRemove = event => {
   event.preventDefault();
 
   const btn = event.currentTarget;
-  // const item = btn.parentElement();
-  const item = btn.closest('.todolist__item');
-  item.remove();
+  removeTaskFromButton(btn);
+};
+
+const onClickTaskList = event => {
+  // console.log({ currentTarget: event.currentTarget, target: event.target });
+  if (event.target && event.target.classList.contains('todolist__item__remove')) {
+    const btn = event.target;
+    removeTaskFromButton(btn);
+  }
 };
 
 const onClickBtnAdd = event => {
@@ -51,3 +66,26 @@ const onKeypressTxtTask = event => {
 
 btnAdd.addEventListener('click', onClickBtnAdd, false);
 txtTask.addEventListener('keypress', onKeypressTxtTask, false);
+
+taskList.addEventListener('click', onClickTaskList);
+
+
+
+// Preload images
+
+const imgLazyload = document.querySelectorAll('.lazyload');
+
+const preload = (element) => {
+  const img = document.createElement('img');
+
+  img.addEventListener('load', event => {
+    element.src = img.src;
+    element.classList.add('loaded');
+  });
+
+  img.src = element.getAttribute('data-src');
+};
+
+Array.prototype.forEach.call(imgLazyload, preload);
+
+
